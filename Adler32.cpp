ff3d040ab0b9255078c0dfbe81596580b93c4a71
Rob Tillaart <rob.tillaart@gmail.com>
2025-08-14 16:48:47 +0200

    add Adler64 (experimental)

--- Adler32.cpp
+++ Adler32.cpp
@@ -1,7 +1,7 @@
 //
 //    FILE: Adler32.cpp
 //  AUTHOR: Rob Tillaart
-// VERSION: 0.2.5
+// VERSION: 0.2.6
 //    DATE: 2022-01-27
 // PURPOSE: Arduino Library for calculating Adler-32 checksum
 //     URL: https://github.com/RobTillaart/Adler
